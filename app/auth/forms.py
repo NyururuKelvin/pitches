e9d8fd0fec76f6a0860472690c9d5f3ffabb3db6
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-21 17:21:16 +0300

    my commit add imports

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import Required,Email,EqualTo
+from wtforms.validators import Required,Email,EqualTo,DataRequired,Length,InputRequired
 from wtforms import ValidationError
 from ..models import User
 
