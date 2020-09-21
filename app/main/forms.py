c201ebe1c2323f3c507ea7ecd567b15fcb60be5e
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-21 11:24:11 +0300

    my commit add pitch classes

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required,length,DataRequired
+from flask_wtf.file import FileField,file_allowed
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    bio = TextAreaField('Tell us about yourself.',validators = [Required()])
     submit = SubmitField('Submit')
+
+class UploadPitch(FlaskForm):
+    category=SelectField('Select Category',validators=[DataRequired()],choices=[('Interview Pitch','Interview Pitch'),
+                                                    ('Products Pitch','Products Pitch'),
+                                                    ('Tech Pitch','Tech Pitch'),
+                                                    ('Political Pitch','Political Pitch'),
+                                                    ('Religious Pitch','Religious Pitch'),
+                                                    ('Pickup Lines','Pickup Lines'),
+                                                    ('Others','None of the above')])
+    pitch=TextAreaField('Write Pitch:',validators=[DataRequired()])
+    submit=SubmitField('Post Pitch')
+  
+class CommentsForm(FlaskForm):
+    comment=TextAreaField('Type comment:', validators=[DataRequired()])
+    submit=SubmitField('Post Comment')
