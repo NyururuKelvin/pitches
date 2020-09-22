2af25f57db767e0be3be536095cec541b2dc4ee6
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-22 12:14:23 +0300

    my commit add key

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MAIL_USERNAME='nyururukelvin99@gmail.com'
 export MAIL_PASSWORD='nyururu'
-export SECRET_KEY='Nyururukey'
+export SECRET_KEY='Flask WTF Secret Key'
 
 python3.6 manage.py server
