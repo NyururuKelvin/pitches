0bdc8617976f5461320a88fa6518588e7ce9ce8c
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-22 11:57:50 +0300

    my commit edit secret key

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MAIL_USERNAME='nyururukelvin99@gmail.com'
 export MAIL_PASSWORD='nyururu'
-export SECRET_KEY='Flask WTF Secret Key'
+export SECRET_KEY='Nyururukey'
 
 python3.6 manage.py server
