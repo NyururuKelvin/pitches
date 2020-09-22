142bd75092b7e032e1322d8c62e16b6ec74d1c72
Nyururukelvin <nyururukelvin9@gmail.com>
2020-09-22 14:08:39 +0300

    my commit rename template & edit data

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MAIL_USERNAME='nyururukelvin99@gmail.com'
-export MAIL_PASSWORD='nyururu'
+export MAIL_PASSWORD='aKP58RJ8UV5Y'
 export SECRET_KEY='Flask WTF Secret Key'
 
 python3.6 manage.py server
